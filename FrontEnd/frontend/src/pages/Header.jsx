9df627b3723514a8a6cfe13053e8c53778041e27
wan-su-2020 <poj5928@gmail.com>
2026-06-25 14:26:44 +0900

    아이콘 깨짐 수정

--- FrontEnd/frontend/src/pages/Header.jsx
+++ FrontEnd/frontend/src/pages/Header.jsx
@@ -1,6 +1,10 @@
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/useAuth';
+
 import logo from '../img/logo.png';
+import icon01 from '../img/icon01.png';
+import icon02 from '../img/icon02.png';
+import icon03 from '../img/icon03.png';
 
 function Header() {
   const navigate = useNavigate();
@@ -12,42 +16,44 @@ function Header() {
   };
 
   return (
-      <header className="header">
-        <div className="header-inner">
-
-          <div
-              className="logo"
-              onClick={() => navigate('/')}
-              style={{ cursor: 'pointer' }}
-          >
-            <img src={logo} alt="로고" />
-          </div>
-
-          <div className="header-icons">
-            <button className="icon-link-btn" onClick={() => navigate('/books')}>
-              <img src="src/img/icon01.png" alt="도서 목록" />
-            </button>
-            <button className="icon-link-btn" onClick={() => navigate('/books/register')}>
-              <img src="src/img/icon02.png" alt="도서 등록" />
-            </button>
-            <button className="icon-link-btn" onClick={() => navigate('/books/deleted')}>
-              <img src="src/img/icon03.png" alt="휴지통" />
-            </button>
-
-            {user ? (
-                <div className="auth-area">
-                  <span>{user.nickname}님 환영합니다</span>
-                  <button className="icon-link-btn" onClick={handleLogout}>로그아웃</button>
-                </div>
-            ) : (
-                <button className="icon-link-btn" onClick={() => navigate('/login')}>
-                  로그인
-                </button>
-            )}
-          </div>
-
+    <header className="header">
+      <div className="header-inner">
+        <div
+          className="logo"
+          onClick={() => navigate('/')}
+          style={{ cursor: 'pointer' }}
+        >
+          <img src={logo} alt="로고" />
         </div>
-      </header>
+
+        <div className="header-icons">
+          <button className="icon-link-btn" onClick={() => navigate('/books')}>
+            <img src={icon01} alt="도서 목록" />
+          </button>
+
+          <button className="icon-link-btn" onClick={() => navigate('/books/register')}>
+            <img src={icon02} alt="도서 등록" />
+          </button>
+
+          <button className="icon-link-btn" onClick={() => navigate('/books/deleted')}>
+            <img src={icon03} alt="휴지통" />
+          </button>
+
+          {user ? (
+            <div className="auth-area">
+              <span>{user.nickname}님 환영합니다</span>
+              <button className="icon-link-btn" onClick={handleLogout}>
+                로그아웃
+              </button>
+            </div>
+          ) : (
+            <button className="icon-link-btn" onClick={() => navigate('/login')}>
+              로그인
+            </button>
+          )}
+        </div>
+      </div>
+    </header>
   );
 }
 
